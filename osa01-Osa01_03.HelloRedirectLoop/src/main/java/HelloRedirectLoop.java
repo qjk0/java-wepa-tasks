aaf03848b0740d61c958f957eeb8b7d215da712a
qjk0 <yamaimo@outlook.com>
2020-08-25 14:28:50 +0300

    Finish task Part01-03 Hello Redirect Loop!

--- osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
+++ osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
@@ -8,6 +8,32 @@ public class HelloRedirectLoop {
 
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
+        int tries = 0;
 
+        while (true) {
+            Socket socket = server.accept();
+
+            Scanner reader = new Scanner(socket.getInputStream());
+            String line = reader.nextLine();
+            System.out.println(line);
+            if (line.contains("quit")) {
+                socket.close();
+                break;
+            }
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println("HTTP/1.1 302 Found");
+            writer.println("Location: http://localhost:8080/");
+            writer.println("");
+            tries++;
+
+            writer.flush();
+
+            reader.close();
+            writer.close();
+            socket.close();
+            System.out.println("Retrying... " + tries);
+        }
     }
+
 }

74e9721368bfaeb77383983667771f51f46617e1
qjk0 <yamaimo@outlook.com>
2020-08-27 16:45:17 +0300

    Finish task 01-11

--- osa01-Osa01_11.Calculator/src/main/java/calculator/CalculatorController.java
+++ osa01-Osa01_11.Calculator/src/main/java/calculator/CalculatorController.java
@@ -8,4 +8,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class CalculatorController {
 
+    @GetMapping("/add")
+    @ResponseBody
+    public String addNumbers(@RequestParam Integer numOne, @RequestParam Integer numTwo) {
+        return String.valueOf(numOne + numTwo);
+    }
+
+    @GetMapping("/multiply")
+    @ResponseBody
+    public String multiplyNumbers(@RequestParam Integer numOne, @RequestParam Integer numTwo) {
+        return String.valueOf(numOne * numTwo);
+    }
+
 }

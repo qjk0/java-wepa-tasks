091872a32f1c3a08eeae4c7c4b831a541ee6e5ef
qjk0 <yamaimo@outlook.com>
2020-08-27 16:00:28 +0300

    Finish task 01-07

--- osa01-Osa01_07.HelloRequestParam/src/main/java/hellorequestparam/HelloRequestParamController.java
+++ osa01-Osa01_07.HelloRequestParam/src/main/java/hellorequestparam/HelloRequestParamController.java
@@ -8,4 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloRequestParamController {
 
+    @GetMapping("/hello")
+    @ResponseBody
+    public String greet(@RequestParam String name) {
+        return (name.equals("") ? "Hello John Doe" : "Hello " + name);
+        
+    }
 }

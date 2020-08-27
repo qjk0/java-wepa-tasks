63ef0798f92f5eec1ed5a9c6981bf22d2d14a4d8
qjk0 <yamaimo@outlook.com>
2020-08-27 15:48:54 +0300

    Finish task 01-06

--- osa01-Osa01_06.HelloPaths/src/main/java/hellopaths/HelloPathsController.java
+++ osa01-Osa01_06.HelloPaths/src/main/java/hellopaths/HelloPathsController.java
@@ -7,5 +7,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloPathsController {
 
+    @GetMapping("/hello")
+    @ResponseBody
+    public String path() {
+        return "Hello";
+    }
+
+    @GetMapping("/paths")
+    @ResponseBody
+    public String route() {
+        return "Paths";
+    }
 
 }

3889b15db142756c27202bea66b20a1f46c6eed7
qjk0 <yamaimo@outlook.com>
2020-08-27 18:38:24 +0300

    Finish task 02-03

--- osa02-Osa02_03.HelloModel/src/main/java/hellomodel/HelloModelController.java
+++ osa02-Osa02_03.HelloModel/src/main/java/hellomodel/HelloModelController.java
@@ -2,10 +2,18 @@ package hellomodel;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloModelController {
+    
+    @GetMapping("/")
+    public String showStoryPage(Model model, @RequestParam String title, @RequestParam String person) {
+        model.addAttribute("title", title);
+        model.addAttribute("person", person);
+        return "index";
+    }
 
 }

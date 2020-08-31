8c4f3393d7ad18664f06643e65d974f5422ec189
qjk0 <yamaimo@outlook.com>
2020-08-31 16:47:25 +0300

    Finish task 02-12

---
+++ osa02-Osa02_12.TodoApplication/src/main/java/todoapplication/Todo.java
@@ -0,0 +1,22 @@
+package todoapplication;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Todo extends AbstractPersistable<Long> {
+
+    private String name;
+    private Integer checked;
+
+}
--- osa02-Osa02_12.TodoApplication/src/main/java/todoapplication/TodoApplicationController.java
+++ osa02-Osa02_12.TodoApplication/src/main/java/todoapplication/TodoApplicationController.java
@@ -1,8 +1,44 @@
 package todoapplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoApplicationController {
-
+    
+    @Autowired
+    private TodoRepository todoRepository;
+    
+    @GetMapping("/")
+    public String list(Model model) {
+        model.addAttribute("items", this.todoRepository.findAll());
+        return "index";
+    }
+    
+    @GetMapping("/wipedb")
+    public String wipedb() {
+        this.todoRepository.deleteAll();
+        return "redirect:/";
+    }
+    
+    @GetMapping("/{id}")
+    public String getSingleTask(Model model, @PathVariable Long id) {
+        Todo todo = this.todoRepository.getOne(id);
+        todo.setChecked(todo.getChecked() + 1);
+        todo = this.todoRepository.save(todo);
+        model.addAttribute("item", todo);
+        return "todo";
+    }
+    
+    @PostMapping("/")
+    public String post(@RequestParam String name) {
+        todoRepository.save(new Todo(name, 0));
+        return "redirect:/";
+    }
+    
 }

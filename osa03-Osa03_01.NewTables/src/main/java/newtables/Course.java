ce311461516947500e7c700ca0727b422066e611
qjk0 <yamaimo@outlook.com>
2020-09-01 15:11:36 +0300

    Finish task 03-01

---
+++ osa03-Osa03_01.NewTables/src/main/java/newtables/Course.java
@@ -0,0 +1,30 @@
+package newtables;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course extends AbstractPersistable<Long> {
+
+    private String name;
+
+    @JoinTable(name = "Enrollment",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+
+    @ManyToMany
+    private List<Student> students = new ArrayList<>();
+
+}
